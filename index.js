6fe8cbf831c5284dbd058707e0e05e9cd30385fe
eacazares <eacazar@gmail.com>
2022-12-02 09:42:45 -0800

    fixed errors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send ('hello world home page')
+    res.render ('home')
 })
 
 app.get('*', (req, res) => {
@@ -16,4 +16,3 @@ app.get('*', (req, res) => {
 })
 
 app.listen(process.env.PORT)
-
