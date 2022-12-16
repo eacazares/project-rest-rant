fd82d26361557ce120292c76cddb70479e638631
eacazares <eacazar@gmail.com>
2022-12-15 19:36:32 -0800

    restrant 4 complete

--- views/default.jsx
+++ views/default.jsx
@@ -5,8 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                {/* BOOTSTRAP CDN LINK (long) */}
-                <link rel="stylesheet" href="/css/style.css"/>
+                {/*<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />*/}
+                <link rel = "stylesheet" href = '/css/style.css'/>
             </head>
             <body>
                 {html.children}
--- views/home.jsx
+++ views/home.jsx
@@ -5,11 +5,16 @@ function home () {
     return (
       <Def>
           <main>
+            <h1>HOME</h1>
+            <div>
+              <img src = "/images/toast.jpg" alt = 'Toast Trio' />
+              <div>
+              Photo by <a href="https://unsplash.com/@ellaolsson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ella Olsson</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+            </div>
             <a href="/places">
-             <button className="btn-primary">Places Page</button>
+              <button className="btn-primary">Places Page</button>
             </a>
-
-              <h1>HOME</h1>
           </main>
       </Def>
     )
